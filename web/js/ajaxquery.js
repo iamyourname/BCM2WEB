c450fcc01bdeaa92aebc56f9f05c3dd772b46572
Michael <iammoiseev@gmail.com>
2020-10-16 17:36:30 -0700

    ll

--- web/js/ajaxquery.js
+++ web/js/ajaxquery.js
@@ -218,12 +218,12 @@ function godBacchus(){
                     "<button onclick=\"myFunction2('Demo3')\" class=\"w3-btn w3-block w3-red\">"+
                     "Ошибка в буфере</button>" +
 
-                    "<div id=\"Demo3\" class=\"w3-hide w3-card w3-padding-16\" style = \"height: 100px\">" +
+                    "<div id=\"Demo3\" class=\"w3-hide w3-card w3-padding-16\" style = \"height: 200px\">" +
                     "<p id=\"textOfError\">" + errorOfGod + "</p>" +
 
                         "<button onclick=\"myFunction2('Demo1')\" class=\"w3-btn w3-block w3-light-blue w3-left-align\">"+
                         "Ответ для пользователя</button>" +
-                        "<div id=\"Demo1\" class=\"w3-hide w3-card w3-padding-16\" style = \"height: 100px\">" +
+                        "<div id=\"Demo1\" class=\"w3-hide w3-card\">" +
                         "<p id=\"textToUserError\">" + TableBufParsed[7] + "</p>" +
                         "</div>" +
 
