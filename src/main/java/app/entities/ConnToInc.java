66f5cd14fa5605524c1bbc7bcd6ba2a6ab848f80
Michael <iammoiseev@gmail.com>
2020-11-03 01:28:50 -0800

    db

--- src/main/java/app/entities/ConnToInc.java
+++ src/main/java/app/entities/ConnToInc.java
@@ -16,6 +16,7 @@ public class ConnToInc {
         Statement stmt = conn.createStatement();
         String sqlDayInfo = "SELECT * FROM RC_DAY_INFO";
         ResultSet rsDay = stmt.executeQuery(sqlDayInfo);
+        rsDay.last();
         for(int i=2;i<10;i++){
             infoLine[i]=rsDay.getString(i);
         }
