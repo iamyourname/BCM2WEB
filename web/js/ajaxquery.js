8a149e891795369225a2494184a9d16f44b54400
Michael <iammoiseev@gmail.com>
2020-11-24 17:18:55 +0300

    d

--- web/js/ajaxquery.js
+++ web/js/ajaxquery.js
@@ -193,7 +193,7 @@ function searchMarkus(){
                 printToMarkus+="</tr>";
 
                 var print2ToMarkus="";
-            printToMarkus+="<tr id=\"Demo101\" class=\"w3-hide\"><td/><td><table>";
+            printToMarkus+="<tr id=\"Demo101\" class=\"w3-hide\"><td><table><tr><td></td></tr></table></td><td><table>";
             printToMarkus+="<thead><tr><th>PLU</th><th>QTY</th></tr></thead>";
 
                 for(var i=0; i < pluQuan.length; i++){
