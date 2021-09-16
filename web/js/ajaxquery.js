eef8389bb631136376789e8ca9e2ddd260b62c8f
M.Moiseev <52793743+iamyourname@users.noreply.github.com>
2021-09-16 18:47:38 +0300

    поиск даже если буфера нет в бд Кадуцей

--- web/js/ajaxquery.js
+++ web/js/ajaxquery.js
@@ -1726,7 +1726,7 @@ function CaduS() {
           // var arrBufFlowInfo=arrInfoMain[7].split("&");
 
 
-            //yyyy.mm.dd
+            //yyyy.mm.ddd
             for(var i=0;i<arrInfo.length-1;i++){
                 var rowInfo = arrInfo[i].split("|");
                     if(rowInfo[0].contains("not_found"))
